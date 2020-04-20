c68393b419a610f1990b83a51517ca267db1fb8e
javad.jahangiri.niopdc <javad.jahangiri.niopdc@gmail.com>
2020-04-20 18:22:09 +0430

    add test class

---
+++ Data/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testprojectaspnetcore3_1.Data
+{
+    public class test
+    {
+        public int Id { get; set; }
+    }
+}
